d11243ba3a6dd230f71e918f14468346bb9cf5e4
myck-prog <myckland.learnprog@gmail.com>
2022-04-23 12:14:58 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { library} from '@fortawesome/fontawesome-svg-core'
+import {faBars, faShoppingCart,faTimes} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBars, faShoppingCart,faTimes)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +19,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+//https://fontawesome.com/v5/docs/web/use-with/react
